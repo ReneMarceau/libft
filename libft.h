8226716bf825cf1a68b6518af732e7fa3c9e3dd4
Rene Marceau <rmarceau@c1r8p4.42quebec.com>
2022-10-19 12:55:25 -0400

    libft.h main.c v1

---
+++ libft.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarceau <rmarceau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/18 14:14:13 by rmarceau          #+#    #+#             */
+/*   Updated: 2022/10/19 12:21:52 by rmarceau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stddef.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+
+void	*ft_memset(void *b, int c, size_t len);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	ft_bzero(void *s, size_t n);
+
+size_t	ft_strlen(const char *s);
+
+#endif
---
+++ main.c
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarceau <rmarceau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/18 14:17:00 by rmarceau          #+#    #+#             */
+/*   Updated: 2022/10/19 12:37:15 by rmarceau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+// int	main(int argc, char **argv)
+// {
+// 	if (argc == 2)
+// 	{
+// 		printf("%d\n", ft_isalpha(argv[1][0]));
+// 		printf("%d\n", isalpha(argv[1][0]));
+// 	}
+// 	return (0);
+// }
+
+// int	main(int argc, char **argv)
+// {
+// 	if (argc == 2)
+// 	{
+// 		printf("%d\n", ft_isdigit(argv[1][0]));
+// 		printf("%d\n", isdigit(argv[1][0]));
+// 	}
+// 	return (0);
+// }
+
+// int	main(int argc, char **argv)
+// {
+// 	if (argc == 2)
+// 	{
+// 		printf("%d\n", ft_isalnum(argv[1][0]));
+// 		printf("%d\n", isalnum(argv[1][0]));
+// 	}
+// 	return (0);
+// }
+
+// int	main(int argc, char **argv)
+// {
+// 	if (argc == 2)
+// 	{
+// 		printf("%d\n", ft_isascii(argv[1][0]));
+// 		printf("%d\n", isascii(argv[1][0]));
+// 	}
+// 	return (0);
+// }
+
+// int	main(int argc, char **argv)
+// {
+// 	if (argc == 2)
+// 	{
+// 		printf("%d\n", ft_isprint(argv[1][0]));
+// 		printf("%d\n", isprint(argv[1][0]));
+// 	}
+// 	return (0);
+// }
+
+// int	main(int argc, char **argv)
+// {
+// 	if (argc == 2)
+// 	{
+//  		printf("%zu\n", ft_strlen(argv[1]));
+// 		printf("%zu\n", strlen(argv[1]));
+//  	}
+//  	return (0);
+// }
+
+// int	main(void)
+// {
+// 	char str1[] = "Wesh la zone!";
+// 	char str2[] = "Wesh la zone!";
+// 	int character = '$';
+// 	size_t len = 3;
+
+// 	printf("%s\n", ft_memset(str1, character, len));
+// 	printf("%s\n", memset(str2, character, len));
+// 	return (0);
+// }
+
+// int	main(void)
+// {
+// 	char str1[] = "Wesh la zone!";
+// 	char str2[] = "Wesh la zone!";
+// 	size_t len = 3;
+
+// 	ft_bzero(str1, len);
+// 	bzero(str2, len);
+// 	printf("%s\n",str1);
+// 	printf("%s\n", str2);
+// 	return (0);
+// }
+
+// int	main(void)
+// {
+// 	// char dst1[100] = "bite";
+// 	// char src1[] = "Wesh la zone!";
+// 	// char dst2[100] = "bite";
+// 	// char src2[] = "Wesh la zone!";
+// 	size_t len = 7;
+
+// 	printf("%s\n", ft_memcpy(((void *)0), ((void *)0), len));
+// 	printf("%s\n", memcpy(((void *)0), ((void *)0), len));
+// 	return (0);
+// }
