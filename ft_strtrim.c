871594c3a6093d3cf835dee7f13d6d728554a937
Rene Marceau <rmarceau@c1r8p7.42quebec.com>
2022-11-01 20:18:10 -0400

    Finished ft_strtrim

---
+++ ft_strtrim.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarceau <rmarceau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/01 11:41:16 by rmarceau          #+#    #+#             */
+/*   Updated: 2022/11/01 17:36:28 by rmarceau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_check_char(char c, char const *set)
+{
+	size_t	i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (set[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	start;
+	size_t	end;
+	char	*str;
+	size_t	str_len;
+
+	if (s1 == NULL || set == NULL)
+		return (NULL);
+	start = 0;
+	while (s1[start] && ft_check_char(s1[start], set))
+		start++;
+	end = ft_strlen(s1);
+	while (end > start && ft_check_char(s1[end - 1], set))
+		end--;
+	str_len = (end - start) + 1;
+	str = (char *)malloc(sizeof(char) * str_len);
+	if (!str)
+		return (NULL);
+	ft_strlcpy(str, s1 + start, str_len);
+	return (str);
+}
