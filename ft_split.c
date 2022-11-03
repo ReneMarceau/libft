047413393510617703e1ac7534d558f4714bbdbe
Rene Marceau <rmarceau@c1r8p5.42quebec.com>
2022-11-03 11:01:57 -0400

    Finished ft_split

---
+++ ft_split.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarceau <rmarceau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/02 11:08:38 by rmarceau          #+#    #+#             */
+/*   Updated: 2022/11/03 10:56:04 by rmarceau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+static size_t	count_words(char const *s, char c)
+{
+	size_t	i;
+	size_t	count;
+
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		if (s[i] != c)
+		{
+			count++;
+			while (s[i] != c && s[i])
+				i++;
+		}
+		else
+			i++;
+	}
+	return (count);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**split;
+	size_t	i;
+	size_t	j;
+	int		index;
+
+	split = (char **)malloc((count_words(s, c) + 1) * sizeof(char *));
+	if (!split)
+		return (NULL);
+	i = 0;
+	j = 0;
+	index = -1;
+	while (i <= ft_strlen(s))
+	{
+		if (s[i] != c && index < 0)
+			index = i;
+		else if ((s[i] == c || i == ft_strlen(s)) && index >= 0)
+		{
+			split[j++] = ft_substr(s, index, i - index);
+			index = -1;
+		}
+		i++;
+	}
+	split[j] = NULL;
+	return (split);
+}
