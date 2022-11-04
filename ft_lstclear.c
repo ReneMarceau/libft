e5602f8072145fcd3662cf1618c59eed97788b06
Rene Marceau <rmarceau@c1r8p5.42quebec.com>
2022-11-04 05:14:17 -0400

    Finished ft_lstclear

---
+++ ft_lstclear.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarceau <rmarceau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/04 04:26:23 by rmarceau          #+#    #+#             */
+/*   Updated: 2022/11/04 05:11:09 by rmarceau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*temp;
+
+	if (lst && del)
+	{
+		while (*lst)
+		{
+			temp = *lst;
+			*lst = temp->next;
+			ft_lstdelone(temp, del);
+		}
+	}
+}
