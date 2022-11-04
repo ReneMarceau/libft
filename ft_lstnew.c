3226908d4b00de91af71c8e287658ab7d27eabbb
Rene Marceau <rmarceau@c1r8p5.42quebec.com>
2022-11-04 01:09:47 -0400

    Finished ft_lstnew

---
+++ ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarceau <rmarceau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/03 18:26:33 by rmarceau          #+#    #+#             */
+/*   Updated: 2022/11/03 21:11:10 by rmarceau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*element;
+
+	element = (t_list *)malloc(sizeof(*element));
+	if (!element)
+		return (NULL);
+	element->content = content;
+	element->next = NULL;
+	return (element);
+}
